8edec45f0a986803317dab43085f4b410054175c
Michael Wu <38211512+mwu412@users.noreply.github.com>
2018-05-10 10:05:31 +0800

    Update DS_Stack.cpp

--- DS_Stack.cpp
+++ DS_Stack.cpp
@@ -1,45 +1,34 @@
-#include <iostream>
-using namespace std;
-
-template <class T>
 class Stack {
 private:
-	T * stack;
+	char* stack;
 	int capacity;
 	int top;
+	void Double() {
+		capacity *= 2;
+		char* newstack = new char[capacity];
+		for (int i = 0; i < capacity / 2; i++) {
+			newstack[i] = stack[i];
+		}
+		delete[]stack;
+		stack = newstack;
+	}
 public:
-	Stack(int capacity = 10) {
-		stack = nullptr;
-		this->capacity = capacity;
+	Stack() {
+		stack = new char[capacity];
+		capacity = 1;
 		top = -1;
 	}
 	bool Empty() {
 		return top == -1;
 	}
-	T&　Top() {
-		if(!Empty()) return stack[top];
+	int Top() {
+		if (!Empty()) return stack[top];
 	}
-	void Push(const T& push) {
-		if (top == capacity - 1) capacity *= 2;
+	void Push(char push) {
+		if (top == capacity - 1) Double();
 		stack[++top] = push;
 	}
-	bool Pop() {
-		if (Empty()) return false;
-		top--;
+	void Pop() {
+		if (!Empty()) top--;
 	}
 };
-
-int main() {
-	Stack<int> s(5);
-	int k = 5;
-	while (k--) {
-		s.Push(k);
-	}
-	for (int i = 0; i < 5; i++) {
-		cout << s.Top() << endl;
-		s.Pop();
-	}
-
-	system("pause");
-	return 0;
-}
